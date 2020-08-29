604b7ec07643ec6e58279705d24adcf50a5eb77b
Izidorio <izidoriojr@gmail.com>
2020-08-29 14:57:18 -0300

    fix: change browser to chromium

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const venom = require('venom-bot');
 
-venom.create().then((client) => start(client));
+const options = {    
+    devtools: false, // Open devtools by default
+    useChrome: false, // If false will use Chromium instance
+    debug: false, // Opens a debug session
+    logQR: true, // Logs QR automatically in terminal    
+    refreshQR: 15000, // Will refresh QR every 15 seconds, 0 will load QR once. Default is 30 seconds
+    autoClose: 60000, // Will auto close automatically if not synced, 'false' won't auto close. Default is 60 seconds (#Important!!! Will automatically set 'refreshQR' to 1000#)
+    disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
+}
+
+venom.create(undefined, undefined, undefined).then((client) => start(client));
 
 function start(client) {
   client.onMessage((message) => {
