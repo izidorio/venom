c7ef7bb169525d65b74c6c351d90adbb3b1eddee
Izidorio <izidoriojr@gmail.com>
2020-08-29 15:09:01 -0300

    fix: add argument

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const options = {
     useChrome: false, // If false will use Chromium instance
     debug: false, // Opens a debug session
     logQR: true, // Logs QR automatically in terminal    
+    browserArgs: ['--no-sandbox', '--disable-setuid-sandbox'], 
     refreshQR: 15000, // Will refresh QR every 15 seconds, 0 will load QR once. Default is 30 seconds
     autoClose: 60000, // Will auto close automatically if not synced, 'false' won't auto close. Default is 60 seconds (#Important!!! Will automatically set 'refreshQR' to 1000#)
     disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
